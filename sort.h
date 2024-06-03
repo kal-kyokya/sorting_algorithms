7be92e426fd0587a0d886c89e17a9fa646015ccf
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-06-03 07:07:44 -0700

    Task 107.

--- sort.h
+++ sort.h
@@ -27,4 +27,10 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 
+void swap_ints(int *a, int *b);
+int hoare_partition(int *array, size_t size, int left, int right);
+void hoare_sort(int *array, size_t size, int left, int right);
+void quick_sort_hoare(int *array, size_t size);
+
+
 #endif /* SORT_H */
