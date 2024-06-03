f3961497c4ca1d5fd7cfdca7788560f2e4f4b9cb
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-06-03 07:29:32 -0700

    Sorting Algorithms.

--- sort.h
+++ sort.h
@@ -6,6 +6,21 @@
 #include <stddef.h>
 #include <stdbool.h>
 
+/* Comparison direction macros for bitonic sort */
+#define UP 0
+#define DOWN 1
+
+/**
+ * enum bool - Enumeration of Boolean values.
+ * @false: Equals 0.
+ * @true: Equals 1.
+ */
+typedef enum bool
+{
+	false = 0,
+	true
+} bool;
+
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -27,10 +42,13 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 
-void swap_ints(int *a, int *b);
-int hoare_partition(int *array, size_t size, int left, int right);
-void hoare_sort(int *array, size_t size, int left, int right);
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
-
 
 #endif /* SORT_H */
